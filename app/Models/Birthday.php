0f2c1f0901636b8f409660a32c12a7809ded68f8
Petr <PPPonomarev@rusatom-utilities.ru>
2021-02-07 22:22:51 +0300

    add relationship

--- app/Models/Birthday.php
+++ app/Models/Birthday.php
@@ -5,11 +5,23 @@ namespace App\Models;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Birthday
+ * @package App\Models
+ */
 class Birthday extends Model
 {
     use CrudTrait;
 
     protected $table = 'birthdays';
     protected $fillable = ['title', 'date'];
+
+    /**
+     * Get the notifications for birthday.
+     */
+    public function notifications()
+    {
+        return $this->hasMany(BirthdayNotification::class);
+    }
 
 }
--- app/Models/BirthdayNotification.php
+++ app/Models/BirthdayNotification.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class BirthdayNotification
+ * @package App\Models
+ */
 class BirthdayNotification extends Model
 {
     protected $table = 'birthday_notifications';
     protected $fillable = ['birthday_id'];
+
+    /**
+     * Get the birthday that owns the notification.
+     */
+    public function birthday()
+    {
+        return $this->belongsTo(Birthday::class);
+    }
 }
