c32df16707f4ef7aa64d9519dfca930948175755
Petr <PPPonomarev@rusatom-utilities.ru>
2021-02-27 16:55:50 +0300

    update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,14 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.autoload({
+    axios: ['axios'],
+    jquery: ['$', 'jQuery', 'window.jQuery'],
+});
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .js('resources/js/app.js', 'public/js/all.min.js')
+    .sass('resources/sass/app.scss', 'public/css/styles.min.css')
+    .options({
+        processCssUrls: false
+    })
+    .version();
