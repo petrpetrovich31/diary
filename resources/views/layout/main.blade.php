d999424da5277e6201b1861cb3c239bc4fabab85
Petr <PPPonomarev@rusatom-utilities.ru>
2021-05-13 10:51:12 +0300

    add meta tags

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Petr Petrovich Петр Петрович">
+        <meta name="robots" content="index, nofollow">
         <title>Petr Petrovich © </title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('/css/styles.min.css') }}">
